032b1812ad5a52acc6881aaef6273ac2db22ab52
Placid Masvidal <contact@placidmasvidal.com>
2022-01-18 05:38:29 -0500

    initial setup

---
+++ src/main/java/com/luv2code/ecommerce/FullStackEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackEcommerceApplication.class, args);
+	}
+
+}
